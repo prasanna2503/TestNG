324e0672a7dadacd3b6ce4b5a39e9ff16eac7598
prasanna2503 <kprasanna2406@gmail.com>
2024-09-25 20:53:08 +0530

    group test cases

---
+++ src/test/java/org/tcs/GroupTest.java
@@ -0,0 +1,23 @@
+package org.tcs;
+
+import org.testng.annotations.Test;
+
+public class GroupTest {
+	
+	@Test(groups = "Smoke" )
+	private void tc2() {
+		
+	}
+	@Test(groups = "reg" )
+	private void tc3() {
+		
+	}
+	@Test(groups = "santiy" )
+	private void tc4() {
+		
+	}
+	@Test(groups = "smoke" )
+	private void tc5() {
+		
+	}
+}
